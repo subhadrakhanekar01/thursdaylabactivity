fb41c767dacdb19ee9b40dc9fa617da710dff2de
SUBHADRA <SUBHADRA@DESKTOP-5KJDC63>
2023-01-06 12:29:31 -0800

    lab8

---
+++ Activity8/src/Question1/Product.java
@@ -0,0 +1,15 @@
+package Question1;
+
+public class Product {
+	int id;		//initialization of variables 
+	String name;
+	int price;
+	public Product(int id, String name, int price) //constructor generated
+	{
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+
+}
